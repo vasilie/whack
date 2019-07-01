2f72d441bcb88c0d66c301277ec4341f7b9bf3c6
vasilie <vasilie.91@gmail.com>
2019-07-01 08:50:15 +0200

    whack init

---
+++ Assets/Scripts/PigSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PigSpawner : MonoBehaviour {
+
+	public GameObject pig;
+	public float initialSpawnTimer = 3;
+	private float spawnTimer;
+	// Use this for initialization
+	void Start () {
+		spawnTimer = initialSpawnTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnTimer-= Time.deltaTime;
+		if (spawnTimer <= 0) {
+			spawnTimer = initialSpawnTimer;
+			spawnEnemy();
+		}
+	}
+
+	void spawnEnemy(){
+		Instantiate(pig, gameObject.transform.position, Quaternion.identity);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	public Animator anim;
+	AudioSource audioData;
+	private bool hit;
+	public float timerInit;
+	private float timer;
+	public AudioClip hitSound;
+    private CapsuleCollider2D capsule;
+	// Use this for initialization
+
+	void Start () {
+		anim = GetComponent<Animator>();
+		audioData = GetComponent<AudioSource>();
+		hit = false;
+		timerInit = 10f;
+		timer = timerInit;
+        capsule = gameObject.GetComponent<CapsuleCollider2D>();
+        capsule.enabled = false;
+    }
+    void HitFalse ()
+    {
+        hit = false;
+        capsule.enabled = false;
+    }
+    void HitTrue()
+    {
+        hit = true;
+        capsule.enabled = true;
+    }
+    void OnTriggerEnter2D (Collider2D other){
+		
+		
+			other.attachedRigidbody.AddForce(new Vector2(1500f, 1400f));
+            capsule.enabled = false;
+            Debug.Log("Whack!");
+			Debug.Log(other.attachedRigidbody);
+			audioData.PlayOneShot(hitSound, 1f);
+		
+
+	
+	}
+	// Update is called once per frame
+	void Update () {
+		
+
+		Debug.Log( hit);
+
+		if (Input.GetKeyDown(KeyCode.Space)){
+			
+			anim.SetBool("space-pressed", true);
+			anim.SetBool("space-released", false);
+		}
+		if (Input.GetKeyUp(KeyCode.Space)){
+			anim.SetBool("space-pressed", false);
+			anim.SetBool("space-released", true);
+			audioData.Play(0);
+
+		}
+	}
+
+
+}
