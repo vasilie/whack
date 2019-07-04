1f91dba4c8d62787433000d497a5af278e85600c
_vasilie <vasilije.milenkovic@diwanee.com>
2019-07-04 16:48:42 +0200

    block

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,10 +14,12 @@ public class Player : MonoBehaviour {
     private CapsuleCollider2D capsule;
     public float strength = 1500f;
     public int damage = 5;
+    public Rigidbody2D rb;
 
 	// Use this for initialization
 
 	void Start () {
+        rb = GetComponent<Rigidbody2D>();
 		anim = GetComponent<Animator>();
 		audioData = GetComponent<AudioSource>();
 		hit = false;
@@ -77,12 +79,27 @@ public class Player : MonoBehaviour {
             HitFalse();
 
 		}
-		if (Input.GetKeyUp(KeyCode.Space)){
-			anim.SetBool("space-pressed", false);
-			anim.SetBool("space-released", true);
-			audioData.Play(0);
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            anim.SetBool("space-pressed", false);
+            anim.SetBool("space-released", true);
+            audioData.Play(0);
 
-		}
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+
+            anim.SetBool("block-pressed", true);
+            rb.mass = 30f;
+
+        }
+        if (Input.GetKeyUp(KeyCode.B))
+        {
+            
+            anim.SetBool("block-pressed", false);
+            rb.mass = 14.2f;
+        }
+
 	}
 
 
